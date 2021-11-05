5d4936e2d3b3f8388b231d9c3404f4561a469a49
kparri <a.parrilla2001@gmail.com>
2021-11-05 05:45:39 +0100

    Implementacion del punto 1 y 2 quitando fallos de "LIMPIAR" con checkbox textarea y textpane y falta de el apartado de formato.

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,15 +1,36 @@
+import sun.security.mscapi.CPublicKey;
+
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import java.awt.*;
 import java.awt.event.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /* La clase JFrame encapsula el concepto de una ventana, para implementar una aplicación que muestre una ventana debemos plantear una clase que herede de la clase JFrame e implemente a a ActionListener para el evento del botón*/
-public class Ventana extends JFrame {
+public class Ventana extends JFrame implements ChangeListener, ActionListener {
 
     /* Definimos variables. */
-    private JTextField textfield1, textfield2, textfield3, textfield4, textfield5, textfield6, textfield7, textfield8, textfield9;
-    private JLabel nombre, apellidos, sexo, direccion, telefono, NIF, codigopost, email, contraseña, pais, provincia, poblacion;
+    private JTextField textfield1, textfield2,textField3, textfield4, textfield5, textfield6, textfield7, textfield8, textfield9;
+    private JLabel nombre, apellidos, sexo, direccion, telefono, NIF, codigopost, email, contraseña, pais, provincia, poblacion, descripcion, carta;
     private JComboBox combo1;
     private JComboBox combo2;
     private JComboBox combo3;
+    private JCheckBox checkBox1, checkBox2, checkBox3, checkBox4, checkBox5;
+    private JTextArea textarea1;
+    private JButton generar;
+    JTextPane text = new JTextPane();
+    private JMenuBar menuBar;
+    private JMenu menu1, menu2, menu3, menu4, menu5;
+    private JMenuItem menuItem21, menuItem22, menuItem31, menuItem32, menuItem33,menuItem34,menuItem35,menuItem51, menuItem52;
+    private  String textoJEditPane;
+
+
+
     /*private String Es;*/
 
 
@@ -18,7 +39,7 @@ public class Ventana extends JFrame {
 
         /* Configuración del JFrame */
         setLayout(null);
-        setBounds(0,0,650,800);
+        setBounds(0,0,750,1000);
         setTitle("Formulario");
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -72,6 +93,16 @@ public class Ventana extends JFrame {
         poblacion.setBounds(10,560,100,30);
         add(poblacion);
 
+        descripcion=new JLabel("Resultado:");
+        descripcion.setBounds(400,45,100,30);
+        add(descripcion);
+
+        carta=new JLabel("Carta de presentación:");
+        carta.setBounds(10,630,150,30);
+        add(carta);
+
+
+
 
 
         /* Campos de texto */
@@ -83,9 +114,9 @@ public class Ventana extends JFrame {
         textfield2.setBounds(120,60,150,30);
         add(textfield2);
 
-        textfield3=new JTextField();
-        textfield3.setBounds(120,110,150,30);
-        add(textfield3);
+        textField3=new JTextField();
+        textField3.setBounds(120,560,150,30);
+        add(textField3);
 
         textfield4=new JTextField();
         textfield4.setBounds(120,160,150,30);
@@ -121,12 +152,12 @@ public class Ventana extends JFrame {
         combo1.addItem("País");
         combo1.addItem("Francia");
         combo1.addItem("España");
-        combo1.addItem("Estados Unidos");
+        combo1.addItem("Rusia");
         combo1.addItem("China");
         combo1.addItem("Italia");
         combo1.addItem("Turquía");
         combo1.addItem("México");
-        combo1.addItem("Tailandia");
+        combo1.addItem("Estados Unidos");
         combo1.addItem("Alemania");
         combo1.addItem("Reino Unido");
 
@@ -136,28 +167,362 @@ public class Ventana extends JFrame {
         combo2.setBounds(120,510,150,30);
         add(combo2);
 
-        /*if (combo1 = "España"){
 
-            combo2.addItem("País");
-            combo2.addItem("Francia");
-            combo2.addItem("España");
-            combo2.addItem("Estados Unidos");
-            combo2.addItem("China");
-            combo2.addItem("Italia");
-            combo2.addItem("Turquía");
-            combo2.addItem("México");
-            combo2.addItem("Tailandia");
-            combo2.addItem("Alemania");
-            combo2.addItem("Reino Unido");
 
-        }
-        */
+
+        combo1.addItemListener(new ItemListener(){
+            public void itemStateChanged(ItemEvent e){
+                combo2.removeAllItems();
+                if (e.getItem() == "Francia"){
+                    combo2.addItem("Provincias");
+                    combo2.addItem("Isla de Francia");
+                    combo2.addItem("Berry");
+                    combo2.addItem("Orleanesado");
+                    combo2.addItem("Normandía");
+                    combo2.addItem("Languedoc");
+                    combo2.addItem("Lyonnais");
+                    combo2.addItem("Delfinado");
+                    combo2.addItem("Champaña");
+                    combo2.addItem("Aunis");
+                    combo2.addItem("Saintonge");
+
+                }else if (e.getItem() == "España"){
+                    combo2.addItem("Provincias");
+                    combo2.addItem("La Rioja");
+                    combo2.addItem("Madrid");
+                    combo2.addItem("Barcelona");
+                    combo2.addItem("Valencia");
+                    combo2.addItem("Sevilla");
+                    combo2.addItem("Alicante");
+                    combo2.addItem("Malaga");
+                    combo2.addItem("Murcia");
+                    combo2.addItem("Cádiz");
+                    combo2.addItem("La Coruña");
+
+                }else if (e.getItem() == "Rusia"){
+                    combo2.addItem("Estados");
+                    combo2.addItem("Adigueya");
+                    combo2.addItem("Altái");
+                    combo2.addItem("Baskortostán");
+                    combo2.addItem("Buriatia");
+                    combo2.addItem("Daguestán");
+                    combo2.addItem("Ingusetia");
+                    combo2.addItem("Kabardia-Balkaria");
+                    combo2.addItem("Kalmukia");
+                    combo2.addItem("Karacháyevo-Cherkesia");
+                    combo2.addItem("Carelia");
+
+                }else if (e.getItem() == "China"){
+                    combo2.addItem("Provincias");
+                    combo2.addItem("Heilongjiang");
+                    combo2.addItem("Jilin");
+                    combo2.addItem("Liaoning");
+                    combo2.addItem("Qinghai");
+                    combo2.addItem("Gansu");
+                    combo2.addItem("Shaanxi");
+                    combo2.addItem("Hebei");
+                    combo2.addItem("Sichuan");
+                    combo2.addItem("Hubei");
+                    combo2.addItem("Henan");
+
+                }else if (e.getItem() == "Italia"){
+                    combo2.addItem("Regiones");
+                    combo2.addItem("Piamonte");
+                    combo2.addItem("Lombardia");
+                    combo2.addItem("Liguria");
+                    combo2.addItem("Valle de Aosta");
+                    combo2.addItem("Véneto");
+                    combo2.addItem("Emilia Romaña");
+                    combo2.addItem("Toscana");
+                    combo2.addItem("Umbría");
+                    combo2.addItem("Molise");
+                    combo2.addItem("Abruzo");
+
+                }else if (e.getItem() == "Turquia"){
+                    combo2.addItem("Provincias");
+                    combo2.addItem("Estambul");
+                    combo2.addItem("Ankara");
+                    combo2.addItem("Esmirna");
+                    combo2.addItem("Bursa");
+                    combo2.addItem("Adana");
+                    combo2.addItem("Gaziantep");
+                    combo2.addItem("Konya");
+                    combo2.addItem("Antalya");
+                    combo2.addItem("Kayseri");
+                    combo2.addItem("Mersin");
+
+                }else if (e.getItem() == "Mexico"){
+                    combo2.addItem("Estados");
+                    combo2.addItem("Aguascalientes");
+                    combo2.addItem("Baja California");
+                    combo2.addItem("Baja California Sur");
+                    combo2.addItem("Campeche");
+                    combo2.addItem("Chiapas");
+                    combo2.addItem("Chihuahua");
+                    combo2.addItem("Ciudad de México");
+                    combo2.addItem("Coahuila de Zaragoza");
+                    combo2.addItem("Colima");
+                    combo2.addItem("Durango");
+
+                }else if (e.getItem() == "Estados Unidos"){
+                    combo2.addItem("Estados");
+                    combo2.addItem("Misisipi");
+                    combo2.addItem("Utah");
+                    combo2.addItem("Vermont");
+                    combo2.addItem("Pensilvania");
+                    combo2.addItem("Nebraska");
+                    combo2.addItem("Washington D. C.");
+                    combo2.addItem("Arizona");
+                    combo2.addItem("Rhode Island");
+                    combo2.addItem("Misuri");
+                    combo2.addItem("Nueva York");
+
+                }else if (e.getItem() == "Alemania"){
+                    combo2.addItem("Provincias");
+                    combo2.addItem("Baden-Wurtemberg");
+                    combo2.addItem("Baviera");
+                    combo2.addItem("Berlín");
+                    combo2.addItem("Brandeburgo");
+                    combo2.addItem("Bremen");
+                    combo2.addItem("Hamburgo");
+                    combo2.addItem("Hesse");
+                    combo2.addItem("Baja Sajonia");
+                    combo2.addItem("Baja Sajonia");
+                    combo2.addItem("Sajonia");
+
+                }else if (e.getItem() == "Reino Unido"){
+                    combo2.addItem("Regiones");
+                    combo2.addItem("Sudeste de Inglaterra");
+                    combo2.addItem("Gran Londres");
+                    combo2.addItem("Noroeste de Inglaterra");
+                    combo2.addItem("Este de Inglaterra");
+                    combo2.addItem("Midlands del Oeste");
+                    combo2.addItem("Sudoeste de Inglaterra");
+                    combo2.addItem("Yorkshire y Humber");
+                    combo2.addItem("Midlands del Este");
+                    combo2.addItem("Nordeste de Inglaterra");
+                    combo2.addItem("Inglaterra");
+
+                }
+                System.out.println(e.getItem());
+
+            }
+        });
+
+
+
+
+
+
+        combo3=new JComboBox();
+        combo3.setBounds(120,110,150,30);
+        add(combo3);
+
+        combo3.addItem("Sexo");
+        combo3.addItem("Hombre");
+        combo3.addItem("Mujer");
+        combo3.addItem("Otro");
+        combo3.addItem("Prefiero no decirlo");
+
+
+
+
+
+
+        //Añado los 3 checkbox con su ChangeListener y la etiqueta
+        checkBox1 = new JCheckBox("Español");
+        checkBox1.setBounds(10,600,75,30);
+        checkBox1.addChangeListener(this);
+        add(checkBox1);
+
+        checkBox2 = new JCheckBox("Inglés");
+        checkBox2.setBounds(100,600,75,30);
+        checkBox2.addChangeListener(this);
+        add(checkBox2);
+
+        checkBox3 = new JCheckBox("Alemán");
+        checkBox3.setBounds(190,600,75,30);
+        checkBox3.addChangeListener(this);
+        add(checkBox3);
+
+        checkBox4 = new JCheckBox("Italiano");
+        checkBox4.setBounds(280,600,75,30);
+        checkBox4.addChangeListener(this);
+        add(checkBox4);
+
+        checkBox5 = new JCheckBox("Otros");
+        checkBox5.setBounds(370,600,75,30);
+        checkBox5.addChangeListener(this);
+        add(checkBox5);
+
+
+
+
+        //Aññado el JTextArea para la descripción
+        textarea1=new JTextArea();
+        textarea1.setBounds(10,660,300,180);
+        JScrollPane scroll = new JScrollPane(textarea1);
+        scroll.setBounds(10,660,300,180);
+        add(scroll);
+
+        //Creación del boton
+        generar=new JButton("Generar");
+        generar.setBounds(10,860,90,35);
+        add(generar);
+
+
+        JTextPane text = new JTextPane(); //Incializamos el textPane
+        setLayout(null); //Layout absoluto
+
+        //Incializamos el scrollPane y le añadimos el editor
+        JScrollPane scrollPane = new JScrollPane(text);
+        scrollPane.setBounds(400,80,300,300);
+        add(scrollPane);
+
+
+		/*Instanciamos una clase SimpleAttributeSet, que guardará los atributos
+		 para un determinado texto: si es negrita, cursiva, fuente, etc.
+		*/
+        SimpleAttributeSet attrs = new SimpleAttributeSet();
+
+
+        generar.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+textoJEditPane = "Nombre: " + textfield1.getText() + "\nApellidos: " + textfield2.getText() + "\nSexo: " + combo3.getSelectedItem() + "\nDirección: " + textfield4.getText() +"\nTeléfono: " + textfield5.getText() +"\nNIF: " + textfield6.getText() +"\nCódigo Postal: " + textfield7.getText() +"\nEmail: " + textfield8.getText() +"\nContraseña: " + textfield9.getText() + "\nPaís: " + combo1.getSelectedItem() + "\nProvincia: " + combo2.getSelectedItem()  + "\nPoblación: " + textField3.getText() + "\nIdiomas: " + checkBox1.getText() + "" + checkBox2.getText() + "" + checkBox3.getText() + "" + checkBox4.getText() + "" + checkBox5.getText() + "\nCarta de presentacion: " + "\n" + textarea1.getText();
+                text.setText(textoJEditPane);
+
+
+
+
+                System.out.println("A");
+            }
+
+
+        });
+
+
+
+        /* Creamos el JMenuBar y lo asociamos con el JFrame */
+        menuBar=new JMenuBar();
+        setJMenuBar(menuBar);
+
+        /* Creamos el primer JMenu y lo pasamos como parámetro al JMenuBar mediante el método add */
+        menu1=new JMenu("Menu");
+        menuBar.add(menu1);
+
+        /* Creamos el segundo y tercer objetos de la clase JMenu y los asociamos con el primer JMenu creado */
+        menu2=new JMenu("Formato");
+        menu1.add(menu2);
+        menu3=new JMenu("Acciones");
+        menu1.add(menu3);
+
+        menu4=new JMenu("Color");
+        menu2.add(menu4);
+        menu5=new JMenu("Formato");
+        menu2.add(menu5);
+
+
+
+        /* Creamos los dos primeros objetos de la clase JMenuItem y los asociamos con el segundo JMenu */
+        menuItem21=new JMenuItem("20");
+        menu2.add(menuItem21);
+        menuItem21.addActionListener(this);
+        menuItem22=new JMenuItem("15");
+        menu2.add(menuItem22);
+        menuItem22.addActionListener(this);
+
+
+        menuItem52=new JMenuItem("Limpiar");
+        menu3.add(menuItem52);
+        menuItem52.addActionListener(this);
+
+        menuItem51=new JMenuItem("Salir");
+        menu3.add(menuItem51);
+        menuItem51.addActionListener(this);
+
+
+
+        /* Creamos los otros dos objetos de la clase JMenuItem y los  asociamos con el tercer JMenu */
+        menuItem31=new JMenuItem("Rojo");
+        menu4.add(menuItem31);
+        menuItem31.addActionListener(this);
+        menuItem32=new JMenuItem("Verde");
+        menu4.add(menuItem32);
+        menuItem32.addActionListener(this);
+        menuItem33=new JMenuItem("Negro");
+        menu4.add(menuItem33);
+        menuItem33.addActionListener(this);
+        menuItem34=new JMenuItem("Azul");
+        menu4.add(menuItem34);
+        menuItem34.addActionListener(this);
+        menuItem35=new JMenuItem("Rosa");
+        menu4.add(menuItem35);
+        menuItem35.addActionListener(this);
 
 
         /* Muestro el JFrame */
         setVisible(true);
 
+
+
+
+
     }
+
+    /* Método que implementa las acciones de cada ítem de menú */
+    public void actionPerformed(ActionEvent e) {
+
+
+        if (e.getSource()==menuItem21) {
+
+
+
+        }
+        if (e.getSource()==menuItem22) {
+
+        }
+        if (e.getSource()==menuItem31) {
+            getContentPane().setBackground(new Color(255,0,0));
+
+        }
+        if (e.getSource()==menuItem32) {
+            getContentPane().setBackground(new Color(0,255,0));
+        }
+        if (e.getSource()==menuItem33) {
+            getContentPane().setBackground(new Color(0,0,0));
+        }
+        if (e.getSource()==menuItem34) {
+            getContentPane().setBackground(new Color(0, 42, 255));
+        }
+        if (e.getSource()==menuItem35) {
+            getContentPane().setBackground(new Color(183, 0, 255));
+        }
+        if (e.getSource()==menuItem51) {
+            System.exit(0);
+        }
+        if (e.getSource()==menuItem52) {
+            textfield1.setText(null);
+            textfield2.setText(null);
+            textfield4.setText(null);
+            textfield5.setText(null);
+            textfield6.setText(null);
+            textfield7.setText(null);
+            textfield8.setText(null);
+            textfield9.setText(null);
+            combo1.setSelectedIndex(0);
+            combo2.setSelectedIndex(0);
+            combo3.setSelectedIndex(0);
+
+
+
+
+        }
+    }
+
+
+
+
 
 
 
@@ -167,5 +532,11 @@ public class Ventana extends JFrame {
         new Ventana();
 
     }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+
+    }
+
 
 }
